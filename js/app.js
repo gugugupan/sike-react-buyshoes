5b157e797062dd353145ab2fb0de647b66b73ce8
gugugupan <gugugupan@163.com>
2015-10-15 16:42:11 +0800

    finish Lesson 8

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 /* js/app.js */
 
-window.onload = function() {
-	console.log("page loaded");
-}
-
 function makeCartScrollNicely() {
 	var cart = document.querySelector( "cart__content" );
 	Ps.initialize(cart);
+}
+
+// 
+window.onload = function() {
+	var $toggle = document.querySelector(".site__right-sidebar-toggle");
+	$toggle.addEventListener("click",function() {
+		document.body.classList.toggle("js-show-right-sidebar");
+	});
 }
